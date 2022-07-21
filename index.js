1d91e40d3210959f4fbe024711fff54ad676b60e
jaurif <jauricflowers@gmail.com>
2022-07-21 09:48:10 -0700

    implementing heroku

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const server = express()
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000);
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+});
 
 const destinations = {
     189723: {
